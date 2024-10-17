9e713758f2e1c39a91eb0eac890bbd252bc85e7c
Léa meynadier <meynadierlea34@gmail.com>
2024-10-17 12:53:52 +0200

    Code
    Code rajouter pour mini jeux plus ou moins

---
+++ Assets/2eme séance/prog 17 octobre.cs
@@ -0,0 +1,64 @@
+using JetBrains.Annotations;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class prog17octobre : MonoBehaviour
+{
+    public TMP_Text messageText;
+    public TMP_InputField numberInput;
+
+    private int randomNumber;
+
+    private void Start()
+    {
+        ResetGame();
+
+    }
+
+    public void Try()
+    {
+        if (string.IsNullOrWhiteSpace(numberInput.text) == true)
+
+        {
+            messageText.text = "Entre un truc valide wesh !";
+            numberInput.text = "";
+            return;
+        }
+
+        if (int.TryParse(numberInput.text, out int nombredujoueur))
+        {
+
+            if (nombredujoueur == randomNumber)
+            { messageText.text = "GOOD JOB"; }
+
+            else if (nombredujoueur > randomNumber)
+
+            { messageText.text = "moins !"; }
+
+            else if (nombredujoueur < randomNumber)
+
+            { messageText.text = "plus !"; }
+
+
+
+            else
+            { 
+                messageText.text = "Entre un truc valide wesh !";
+                numberInput.text = "";
+            }
+        }
+    }
+    public void ResetGame()
+
+    {
+        randomNumber = Random.Range(1, 100 + 1);
+        Debug.Log("Generated number: " + randomNumber);
+        messageText.text = "Devine le nombre ou je te tape !"; 
+    }
+}
+
+
+
+
+
